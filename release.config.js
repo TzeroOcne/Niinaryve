cc8029852d237aea343227a631eb9485b527320c
Tzero Ocne <tzeroocne.interface@gmail.com>
2023-06-30 04:37:15 +0700

    feat(open): add script to open component

--- release.config.js
+++ release.config.js
@@ -1,12 +1,17 @@
 export default {
   plugins: [
-    '@semantic-release/github',
-    {
-      assets: [
-        { path: 'dist/main.css', label: 'App CSS distribution' },
-        { path: 'dist/main.js', label: 'App JS distribution' }
-      ]
-    }
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'dist/main.css', label: 'App CSS distribution' },
+          { path: 'dist/main.js', label: 'App JS distribution' }
+        ]
+      }
+    ]
   ],
   branches: 'main',
 };
