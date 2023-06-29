983a41721ffdb60fd399c43435563309d0d5033e
Tzero Ocne <tzeroocne.interface@gmail.com>
2023-06-30 04:26:34 +0700

    build(github): add github release plugin

--- release.config.js
+++ release.config.js
@@ -1,3 +1,12 @@
 export default {
+  plugins: [
+    '@semantic-release/github',
+    {
+      assets: [
+        { path: 'dist/main.css', label: 'App CSS distribution' },
+        { path: 'dist/main.js', label: 'App JS distribution' }
+      ]
+    }
+  ],
   branches: 'main',
 };
