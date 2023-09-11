9366004cb0ea759ac782ee68e9b0d9d3ebc12d81
Tzero Ocne <tzeroocne.interface@gmail.com>
2023-09-11 23:37:33 +0700

    chore(setup): add crx for extension development

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+    {
+      'files': ['*.svelte'],
+      'parser': 'svelte-eslint-parser',
+      'parserOptions': {
+        'parser': '@typescript-eslint/parser',
+      },
+    },
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'extraFileExtensions': ['.svelte'],
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { crx } from '@crxjs/vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    crx({
+      manifest: {
+        manifest_version: 3,
+        name: 'niinaryve',
+        version: '2.0.0',
+      },
+    }),
+  ],
+});
