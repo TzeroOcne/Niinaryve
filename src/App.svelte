a3be420c4ecdfca93c136ebe8990b87b3295f99c
Tzero Ocne <tzeroocne.interface@gmail.com>
2023-09-12 21:02:05 +0700

    chore: vite web extension plugin setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import './index.css';
+  import './app.css';
 
   let baseColor: string = '#000000';
   let memberColor: string = '#000000';
@@ -16,9 +16,14 @@
 </script>
 
 <main>
-  <input bind:value={baseColor} type="color" name="" id="">
-  <input bind:value={memberColor} type="color" name="" id="">
-  <input bind:value={adminColor} type="color" name="" id="">
+  <div class="grid grid-cols-2 w-32 gap-y-2 gap-x-8">
+    <label for="base">Base</label>
+    <input bind:value={baseColor} type="color" name="base" id="">
+    <label for="member">Member</label>
+    <input bind:value={memberColor} type="color" name="member" id="">
+    <label for="Admin">Admin</label>
+    <input bind:value={adminColor} type="color" name="admin" id="">
+  </div>
 </main>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,45 @@
-import { crx } from '@crxjs/vite-plugin';
+import webExtension from '@samrum/vite-plugin-web-extension';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 import { defineConfig } from 'vite';
+
+const youtubeLivechatURLPattern = 'https://*.youtube.com/*';
+// const youtubeURLPattern = 'https://*.youtube.com/*';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    crx({
+    webExtension({
       manifest: {
-        manifest_version: 3,
         name: 'niinaryve',
+        manifest_version: 3,
         version: '2.0.0',
+        action: {
+          default_popup: 'index.html',
+        },
+        content_scripts: [
+          {
+            run_at: 'document_start',
+            js: [
+              'src/scripts/content-scripts.ts',
+            ],
+            matches: [
+              youtubeLivechatURLPattern,
+            ],
+            all_frames: true,
+          },
+        ],
+      },
+      additionalInputs: {
+        scripts: [
+        ],
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
 });
